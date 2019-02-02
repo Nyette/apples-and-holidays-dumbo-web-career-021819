0bbbbef53fd38693bb71af40af1da4126ba37d0b
Antoinette Lee <antoinette.lee02@myhunter.cuny.edu>
2019-02-02 03:34:37 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -62,8 +62,11 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
+  holiday_supplies.collect do |holiday, item|
+  end
 end
+
+
 
 
 
